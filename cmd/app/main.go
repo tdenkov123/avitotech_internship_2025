18db36f7d355146a7449c30159134ffbcf60fc13
TDENKOV <tdenkov123@mail.ru>
2025-11-20 21:18:32 +0300

    added logger and envconf in main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"github.com/tdenkov123/avitotech_internship_2025/internal/config"
+	"github.com/tdenkov123/avitotech_internship_2025/internal/logger"
+)
+
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	logg, err := logger.New(cfg.LogLevel)
+	if err != nil {
+		log.Fatalf("failed to init logger: %v", err)
+	}
+	defer logg.Sync()
 }
