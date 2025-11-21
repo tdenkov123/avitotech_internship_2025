21a1a963f8c86ad626bdfdd6656455bfcc1cfc5c
TDENKOV <tdenkov123@mail.ru>
2025-11-21 14:53:17 +0300

    updated main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,9 +6,11 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/tdenkov123/avitotech_internship_2025/internal/config"
 	httpserver "github.com/tdenkov123/avitotech_internship_2025/internal/http_server"
 	"github.com/tdenkov123/avitotech_internship_2025/internal/logger"
+	"github.com/tdenkov123/avitotech_internship_2025/internal/service"
 	"go.uber.org/zap"
 )
 
@@ -24,10 +26,17 @@ func main() {
 	}
 	defer logg.Sync()
 
-	srv := httpserver.New(cfg, logg)
-
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+
+	dbPool, err := pgxpool.New(ctx, cfg.DatabaseURL)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer dbPool.Close()
+
+	svc := service.New(dbPool)
+	srv := httpserver.New(cfg, logg, svc)
 
 	logg.Info("starting HTTP server", zap.String("port", cfg.ServerPort))
 
